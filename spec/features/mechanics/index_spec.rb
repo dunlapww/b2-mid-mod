6c9a065b85a04215a005baf5b92323fadaede157
Will Dunlap <dunlapww@gmail.com>
2020-10-23 09:52:43 -0600

    Add mechanics index view and pass feature test

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -14,14 +14,14 @@ describe 'As a user' do
   describe 'When I vist a mechanics index page' do
     before(:each) do
       @mechanic1 = Mechanic.create({name: "Earl", years: 3})
-      @mechanic1 = Mechanic.create({name: "Steve", years: 6})
+      @mechanic2 = Mechanic.create({name: "Steve", years: 6})
     end
     it "I see a header saying all 'All Mechanics' and a list of their names and years of experience" do
       visit '/mechanics'
 
       expect(page).to have_content("All Mechanics")
-      [@mechanic1, @mechanic2].each do
-        within("#mechanic - #{mechanic.id}") do
+      [@mechanic1, @mechanic2].each do |mechanic|
+        within("#mechanic-#{mechanic.id}") do
           expect(page).to have_content("#{mechanic.name} - #{mechanic.years} years of experience")
         end
       end
